47196673f4578fc8c3c447651da12238a4d00e4d
Brian Emmanuel Magario <brian.magario@gmail.com>
2022-06-04 14:03:46 -0300

    Docker improvements

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-## Create Docker
+## Create a DOCKERHOST variable to connect between dockers.
+# In linux: 
+    # export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
+## Create Dockers
 docker-compose up -d --build
 
 ## Watch Docker's Logs
 docker logs -f <DOCKER_HASH>
 
-## Open Docker
+## Open Docker Bash
 docker exec -it <DOCKER_NAME> bash
+
+
+## Exec Mariadb Commands
+docker exec -it <DOCKER_NAME> mysql -h db -u <DB_USER> -p<DB_PASS> <DB_NAME_DEVELOPMENT> -e "SELECT * FROM <TABLE>;"
--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,28 +1,30 @@
-import { Sequelize } from 'sequelize-typescript';
-import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../shared/constants';
-import { databaseConfig } from './database.config';
-import { User } from 'src/users/user.entity';
+import { Sequelize } from "sequelize-typescript";
+import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from "../shared/constants";
+import { databaseConfig } from "./database.config";
+import { User } from "src/users/user.entity";
 
-export const databaseProviders = [{
-    provide: SEQUELIZE,
-    useFactory: async () => {
-        let config;
-        switch (process.env.NODE_ENV) {
-        case DEVELOPMENT:
-           config = databaseConfig.development;
-           break;
-        case TEST:
-           config = databaseConfig.test;
-           break;
-        case PRODUCTION:
-           config = databaseConfig.production;
-           break;
-        default:
-           config = databaseConfig.development;
-        }
-        const sequelize = new Sequelize(config);
-        sequelize.addModels([User]);
-        await sequelize.sync();
-        return sequelize;
-    },
-}];
+export const databaseProviders = [
+	{
+		provide: SEQUELIZE,
+		useFactory: async () => {
+			let config;
+			switch (process.env.NODE_ENV) {
+				case DEVELOPMENT:
+					config = databaseConfig.development;
+					break;
+				case TEST:
+					config = databaseConfig.test;
+					break;
+				case PRODUCTION:
+					config = databaseConfig.production;
+					break;
+				default:
+					config = databaseConfig.development;
+			}
+			const sequelize = new Sequelize(config);
+			sequelize.addModels([User]);
+			await sequelize.sync();
+			return sequelize;
+		},
+	},
+];
